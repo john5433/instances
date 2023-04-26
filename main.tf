5ee3fbbd43d78c5f5eb39f431a3bca880c453c0a
James Bond <jamesbond@gmail.com>
2023-04-26 05:58:56 +0000

    end of lab

--- main.tf
+++ main.tf
@@ -6,4 +6,12 @@ data "terraform_remote_state" "network_details" {
     region = "us-east-2"
   }
 }
+resource "aws_instance" "my_vm" {
+ami = "ami-06c4532923d4ba1ec"
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+instance_type = "t3.micro"
+tags = {
+Name = "student.3-vm1"
+  }
+}
 
