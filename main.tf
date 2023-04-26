17a836b0637ca967433ca99d2c12b8dd5811068b
James Bond <jamesbond@gmail.com>
2023-04-26 09:42:19 +0000

    End of SSH Keys and Security Groups Lab

--- main.tf
+++ main.tf
@@ -8,8 +8,10 @@ data "terraform_remote_state" "network_details" {
 }
 resource "aws_instance" "my_vm" {
 ami = "ami-06c4532923d4ba1ec"
-subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
 instance_type = "t3.micro"
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+key_name = data.terraform_remote_state.network_details.outputs.aws_key_pair
+vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
 tags = {
 Name = "student.3-vm1"
   }
